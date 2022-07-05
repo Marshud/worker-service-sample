2efc1bb0325b33f61afdb1af3b9ca35cdf5c6f87
Marshud <ubatamye@gmail.com>
2022-07-05 23:54:55 +0300

    Restructing API implementation with DRY

--- index.php
+++ index.php
@@ -3,11 +3,14 @@ include $_SERVER['DOCUMENT_ROOT']."/inc/connection.php";
 
 // Router
 use Steampixel\Route;
-use Shift\Old\Shift;
+use Shift\Shift;
+use Shift\Timer\Timer;
+use Shift\Worker\Worker;
 
 
-$shift = new Shift;
+// $shift = new Shift;
 $payload = json_decode(file_get_contents('php://input'), true); 
+$obj = new stdClass();
 
 include $_SERVER['DOCUMENT_ROOT']."/api/index.php";
 
@@ -29,28 +32,56 @@ Route::add('/viewshift', function() {
 
 
 /**
- * Endpoints
+ * API Endpoints
  */
 
-Route::add('/api/addworker', function() use($payload, $shift) {
+Route::add('/api/startshift', function() use($payload, $obj) {
   header('Content-Type: application/json');
-  return addWorkerApi();  
+  $email = $payload['email'];
+  if(filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    $shift = new Shift($email);
+    $obj->status = $shift->startShift();
+  } else {
+    $obj->status = false;
+  }
+
+  return json_encode($obj);
 }, 'post');
 
-Route::add('/api/getshift', function() use($payload, $shift) {
+Route::add('/api/addworker', function() use($payload, $obj) {
   header('Content-Type: application/json');
-  return getShiftApi();  
+  $email = $payload['email'];
+  if(filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    $name = $payload['name'];
+    $worker = new Worker($email);
+    $obj->status = $worker->addWorker($name);
+  } else {
+    $obj->status = false;
+  }
+  return json_encode($obj);  
 }, 'post');
 
-Route::add('/api/startshift', function() use($payload, $shift) {
+Route::add('/api/getshift', function() use($payload, $obj) {
   header('Content-Type: application/json');
-  return startShift();  
+  $email = $payload['email'];
+  if(filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    $name = $payload['name'];
+    $shift = new Shift($email);
+    $obj->status = $shift->getShift($name);
+  } else {
+    $obj->status = false;
+  }
+  return json_encode($obj); 
 }, 'post');
 
-Route::add('/api/gettime', function() use($payload, $shift) {
+
+
+Route::add('/api/gettime', function() use($payload, $obj) {
   // Count real time
+  $timer = new Timer;
   header('Content-Type: application/json');
-  return getTime();  
+  $obj->time = $timer->runningTime();
+  return json_encode($obj);  
 }, 'post');
 
 // Run the router
